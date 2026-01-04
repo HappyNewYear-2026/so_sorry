170d6cfe5837b24f347f8492649457a727f0b14e
your Ashish <hackz.talks@gmail.com>
2026-01-04 15:48:28 +0530

    DiaryPage.jsx

--- src/components/DiaryPage.jsx
+++ src/components/DiaryPage.jsx
@@ -32,7 +32,7 @@ export default function DiaryPage({ setCurrentPage }) {
           transition={{ duration: 1, delay: 0.5 }}
           className="text-center"
         >
-          <h1 className="text-3xl text-pink-300 mb-8 leading-relaxed">I'm so sorry Ritu…😞</h1>
+          <h1 className="text-3xl text-pink-300 mb-8 leading-relaxed">I'm sorry Ritu....🥺</h1>
 
           <motion.div
             initial={{ opacity: 0, y: 20 }}
